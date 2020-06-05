7911ce1065b4e5e0af59e1020af6d026264f6ed0
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2020-06-05 17:35:56 +0700

    delete build

--- build/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleBadRequest = void 0;
-function handleBadRequest(req, res) {
-    return res.status(400);
-}
-exports.handleBadRequest = handleBadRequest;
