271a2ff9d0a867ded74157d157388aa92b306309
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2020-06-07 22:05:45 +0700

    fix pagination page_size not preventing 0 or negative value

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -14,10 +14,12 @@ export function paging(req: Request, res: Response, next: NextFunction) {
     req.query
   );
   const page = Math.max(_page, 1);
-  const pageSize = Math.min(_page_size, 100);
+  const pageSize = Math.max(0, Math.min(_page_size, 100));
 
   data.offset = page * pageSize - (pageSize - 1) - 1;
   data.limit = pageSize;
   (res as any).locals.page = data;
   return next();
 }
+
+// export function validate
