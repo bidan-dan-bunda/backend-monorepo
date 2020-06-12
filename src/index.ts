1509a93958c680ad37e1b0fe6c58916a3a702d59
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2020-06-12 15:19:35 +0700

    increase request timeout

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import http from 'http';
 import app from './app';
 import d from 'debug';
+import ms from 'ms';
 
 const port = process.env.PORT || 3000;
 const debug = d('app:server');
 const server = http.createServer(app);
+server.setTimeout(ms('30m'));
+
 server.listen(port, () => {
   debug('listening on port %d', port);
 });
