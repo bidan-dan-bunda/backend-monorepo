a09a20341fac0cee8733f6719bf9d943c16c58a3
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2020-06-26 10:12:58 +0700

    exit process on SIGINT

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const debug = d('app:server');
 const server = http.createServer(app);
 server.setTimeout(ms('30m'));
 
+process.on('SIGINT', () => {
+  process.exit();
+});
+
 server.listen(port, () => {
   debug('listening on port %d', port);
 });
